17f8516c3640f06b9b7a385310fd95b8c4cbee37
Alejandro Minor <alejandrominor@outlook.com>
2024-02-16 20:22:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 HERE = pathlib.Path(__file__).parent
 
 VERSION = '0.0.1' 
-PACKAGE_NAME = 'odoo_db_restore' 
+PACKAGE_NAME = 'OdooDBRestore' 
 AUTHOR = 'Alejandro Minor' 
 URL = 'https://github.com/alejandrominor' 
 
